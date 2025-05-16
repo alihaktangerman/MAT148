86f5ec9a3c0758c7d573edfff15e4885b5b39363
Ali Haktan <107588410+alihaktangerman@users.noreply.github.com>
2025-05-16 08:31:29 +0300

    Create pairings.cpp

---
+++ pairings.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<bool> used(6,false);
+vector<pair<int,int>> tables;
+int counter = 0;
+
+void solve() {
+    auto p1 = 0;
+    while (used[p1] && p1!=6) p1++;
+    if (p1 != 6) {
+        for (auto p2 = p1+1; p2 != 6; p2++) {
+            if (!used[p2]) {
+                used[p1] = true;
+                used[p2] = true;
+                tables.push_back(pair<int,int>{p1+1,p2+1});
+                solve();
+                used[p1] = false;
+                used[p2] = false;
+                tables.pop_back();
+            }
+        }
+    } else {
+        for (auto a: tables) {
+            cout << "(" << a.first << " " << a.second << ")" << " ";
+        }
+        cout << endl;
+        counter++;
+    }
+}
+
+int main() {
+    solve();
+    cout << counter << endl;
+}
